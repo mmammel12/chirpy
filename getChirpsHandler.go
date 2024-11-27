74c26805733287594276727c86d85a3f30d6a786
Marty Mammel <mmammel12@gmail.com>
2024-11-27 13:15:39 -0600

    ch 9

--- getChirpsHandler.go
+++ getChirpsHandler.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"slices"
+
+	"github.com/google/uuid"
+	"github.com/mmammel12/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.ListChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+	sort := r.URL.Query().Get("sort")
+	if sort == "" {
+		sort = "asc"
+	}
+
+	if sort != "asc" && sort != "desc" {
+		respondWithError(w, http.StatusBadRequest, "Invalid sort", fmt.Errorf("Invalid sort query param: %v", sort))
 		return
 	}
 
+	authorIDQueryParam := r.URL.Query().Get("author_id")
+	var chirps []database.Chirp
+	var err error
+	if authorIDQueryParam != "" {
+		authorID, err := uuid.Parse(authorIDQueryParam)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author_id", err)
+			return
+		}
+		chirps, err = cfg.db.ListChirpsByAuthor(r.Context(), authorID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps by author", err)
+			return
+		}
+	} else {
+		chirps, err = cfg.db.ListChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
+	}
+
 	jsonChirps := []Chirp{}
-	for _, dbChirp := range dbChirps {
+	for _, dbChirp := range chirps {
 		jsonChirps = append(jsonChirps, Chirp{
 			ID:        dbChirp.ID,
 			CreatedAt: dbChirp.CreatedAt,
@@ -20,6 +51,10 @@ func (cfg *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
 			UserID:    dbChirp.UserID,
 			Body:      dbChirp.Body,
 		})
+	}
+
+	if sort == "desc" {
+		slices.Reverse(jsonChirps)
 	}
 
 	respondWithJSON(w, http.StatusOK, jsonChirps)
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -16,6 +16,11 @@ DELETE FROM chirps;
 SELECT * FROM chirps
 ORDER BY created_at;
 
+-- name: ListChirpsByAuthor :many
+SELECT * FROM chirps
+WHERE user_id = $1
+ORDER BY created_at;
+
 -- name: GetChirpById :one
 SELECT * FROM chirps
 WHERE id = $1;
